11f6494ce5b499eb474e75c95ae2f5263ede52c1
munir-v <81745551+munir-v@users.noreply.github.com>
2022-09-28 11:50:56 -0700

    feat: add new todo list item

---
+++ new_todo_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import os
+from pathlib import Path #used to get working directory
+
+todolist_path = Path(os.path.expanduser('~/Documents/Main Obsidian Vault/To-do List.md'))
+# todolist_path = Path(os.path.expanduser('~/Downloads/To-do List.md'))
+
+def add_todo_item():
+	file = []
+	with open(todolist_path, "r") as f:
+		file = f.readlines()
+		for i, line in enumerate(file):
+			if "### At Computer" in line:
+				todo_item = str(os.environ["KMVAR_Item"])
+				file.insert(i+1,"- [ ] " + todo_item + "\n")
+				break
+	with open(todolist_path, "w") as f: 
+		f.writelines(file)
+
+add_todo_item()
