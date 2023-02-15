aa4b48d1002ae5d1f9e70d667549d32c3a141d66
munir-v <81745551+munir-v@users.noreply.github.com>
2023-02-14 17:05:07 -0800

    fix: todo line not found

--- new_todo_item.py
+++ new_todo_item.py
@@ -8,11 +8,14 @@ def add_todo_item():
 	file = []
 	with open(todolist_path, "r") as f:
 		file = f.readlines()
+		todo_item = str(os.environ["KMVAR_Item"])
 		for i, line in enumerate(file):
 			if "### To-do" in line:
-				todo_item = str(os.environ["KMVAR_Item"])
 				file.insert(i+1,"- [ ] " + todo_item + "\n")
 				break
+		else:
+				file.insert(0,"- [ ] " + todo_item + "\n")
+
 	with open(todolist_path, "w") as f: 
 		f.writelines(file)
 
