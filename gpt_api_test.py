77533a7804e9808f98f449063a6b88d87fb8b167
munir-v <81745551+munir-v@users.noreply.github.com>
2023-04-14 18:55:20 -0700

    gpt test

---
+++ gpt_api_test.py
@@ -0,0 +1,31 @@
+import openai
+import os
+import Constants
+
+# Set up the OpenAI API client
+api_key = Constants.GPT_API_KEY
+openai.api_key = api_key
+
+
+# Use the API key in your code
+response = requests.get('https://api.example.com', headers={'Authorization': f'Bearer {api_key}'})
+
+
+def ask_gpt(question):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"{question}\nAnswer:",
+        temperature=0.5,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    answer = response.choices[0].text.strip()
+    return answer
+
+if __name__ == "__main__":
+    question = input("Please enter your question: ")
+    answer = ask_gpt(question)
+    print(f"Answer: {answer}")
