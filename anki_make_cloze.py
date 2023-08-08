5db3bc00630cb3b1c078b4dd98925c380a24ac28
munir-v <81745551+munir-v@users.noreply.github.com>
2023-08-08 18:23:34 -0400

    feat: start cloze from selected number

--- anki_make_cloze.py
+++ anki_make_cloze.py
@@ -2,28 +2,28 @@
 
 import os
 
-input = str(os.environ["KMVAR_Input"])
+def make_cloze():
+	cloze_counter = int(os.environ["KMVAR_StartFromCloze"])
 
-def make_cloze(input):
-	cloze_counter = 2
+	input = str(os.environ["KMVAR_Input"])
 	input.strip()
 	input_words = input.split()
 
 	for i, word in enumerate(input_words): # add spaces back
 		input_words[i] = word + ' '
 
-	input_words.insert(0,'{{c1::')
+	input_words.insert(0,'{{c' + str(cloze_counter) + '::')
 
 	for i, word in enumerate(input_words):
 		words_per_cloze = int(os.environ["KMVAR_Words"])
 		if i>1 and i % (words_per_cloze + 1) == 0:
-			input_words.insert(i,'}} {{c' + str(cloze_counter) + '::')
 			cloze_counter += 1
+			input_words.insert(i,'}} {{c' + str(cloze_counter) + '::')
 	input_words.append('}}')
 
 	result = ''.join(input_words)
 	return result.replace(' }}','}}')
 
-print(make_cloze(input))
+print(make_cloze())
 
 
