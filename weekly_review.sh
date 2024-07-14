b2d2c854c99938b915318432eb4e7942b3900d37
munir-v <81745551+munir-v@users.noreply.github.com>
2024-07-14 13:20:11 -0400

    gitignore

--- weekly_review.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ~/Documents/Random\ Stuff/signal-export && ./sigexport.py EXPORT --old "~/Documents/Random Stuff/signal-export/EXPORT" --overwrite
-brew update && brew upgrade && pip3 install --upgrade pip
-
