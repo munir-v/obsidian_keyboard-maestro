08cf00be40d33678527afdd2451702f9525a7538
munir-v <81745551+munir-v@users.noreply.github.com>
2024-11-08 11:28:40 -0800

    enhance Keyboard Maestro Python template: read input from stdin and write to file

--- TEMPLATE_keyboard_maestro_python.py
+++ TEMPLATE_keyboard_maestro_python.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 import os
+import sys
 
 def example():
-	example_variable = str(os.environ["KMVAR_Variable_Name"])
+	# access local, global, and instance variables
+	example_variable = str(os.environ["KMVAR_Variable_Name"]) 
+	
+	# access the input text from the Keyboard Maestro action
+	input_text = sys.stdin.read()
 
-	with open("/Users/USER/Downloads/TEST.txt", "x") as f:
-		f.write(example_variable)
+	with open(os.path.expanduser("~/Downloads/TEST.txt"), "x") as f:
+		f.write(example_variable + "\n")
+		f.write(input_text)
 
 example()
 
--- test.py
+++ test.py
@@ -1,7 +1,22 @@
-# import matplotlib.pyplot as plt
-print("TEST")
-# import numpy as np
+#!/usr/bin/env python
+import sys
+import os
 
-# values = np.random.rand(100)
-# plt.hist(values, bins=10)
-# plt.show()
+def example():
+	# example_variable = str(os.environ["KMVAR_PW_test"])
+	# example_variable = str(os.environ["KMINFO_Trigger"])
+	example_variable = str(os.environ["KMVAR_TEST"])
+	example_variable2 = str(os.environ["KMINFO_TriggerValue"])
+	input_text = sys.stdin.read()
+	# example_variable = str(os.environ["KMVAR_local_test"])
+
+	with open(os.path.expanduser("~/Downloads/TEST.txt"), "w") as f:
+		f.write(example_variable + "\n\n")
+		f.write(example_variable2 + "\n\n")
+		f.write(input_text)
+
+example()
+
+# Execute the following from an "Execute Shell Script" action in Keyboard Maestro
+# with input from text to pass multiple variables
+# $(which python) /Users/USER/Downloads/keyboard_maestro_python_example.py
